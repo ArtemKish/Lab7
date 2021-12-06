8dd5823931c823f15d20c8f1cba72cacc9bf5e8e
Acer <kishart2003@yandex.ru>
2021-12-06 11:06:51 +0300

    lab 4

---
+++ Lab1/ConsoleApp1/ConsoleApp1/Kishmariya/KishmariyaException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kishmariya
+{
+    class KishmariyaException : Exception
+    {
+        public KishmariyaException(string message) : base(message) { }
+    }
+}
