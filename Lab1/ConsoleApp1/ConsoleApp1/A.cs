e39657ce5833bb6df84c6a3b2db5922845b29bdd
Acer <kishart2003@yandex.ru>
2021-12-06 10:28:08 +0300

    lab 2

---
+++ Lab1/ConsoleApp1/ConsoleApp1/A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class A
+    {
+        protected double[] x;
+
+        public double[] Solvex1(double a, double b)
+        {
+            if (a == 0)
+            {
+                throw new Exception("Корней нет");
+            }
+            return x = new double[] { (-b/a) };
+        }
+    }
+}
---
+++ Lab1/ConsoleApp1/ConsoleApp1/B.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class B:A
+    {
+        protected double getDis(double a, double b, double c)
+        {
+            return b * b - (4 * a * c);
+        }
+
+        protected double[] Solvex2(int a, int b, int c)
+        {
+            if (a == 0)
+            {
+                return Solvex1(b, c);
+            }
+
+            double D = getDis(a, b, c);
+
+            if (D == 0)
+            {
+                return x = new double[] { -b / (2 * a) };
+            }
+
+            if (D > 0)
+            {
+                D = Math.Sqrt(D);
+                return x = new double[] {
+                    (-b + D) / (2 * a),
+                    (-b - D) / (2 * a)
+                };
+            }
+
+            throw new Exception("Корней нет");
+
+        }
+    }
+}
